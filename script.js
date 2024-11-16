94472e8abafed45ce877cba905da52745a72a129
GedeDipa <yulistyadewi89@gmail.com>
2024-11-16 17:27:47 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const motto = document.querySelector('.motto');
+    motto.addEventListener('click', function() {
+        alert('Embrace simplicity!');
+    });
+});
